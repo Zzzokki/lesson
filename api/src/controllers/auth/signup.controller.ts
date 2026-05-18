9f8cf40bca8e61de7ce313a1d61add13bb728822
Zzzokki <dispositionz30k@gmail.com>
2026-05-18 15:04:58 +0800

    hub

--- api/src/controllers/auth/signup.controller.ts
+++ api/src/controllers/auth/signup.controller.ts
@@ -4,18 +4,31 @@ import { db } from "../../database/index.js";
 import { users } from "../../database/schema/user.schema.js";
 
 export const signUpController: RequestHandler = async (req, res) => {
-  const { body } = req;
+  try {
+    const { body } = req;
 
-  const { username, email, password } = body;
+    const { username, email, password } = body;
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: "Missing required fields" });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const existingUser = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.email, email),
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const [user] = await db
+      .insert(users)
+      .values({ username, password, email })
+      .returning();
+
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  const user = await db
-    .insert(users)
-    .values({ username, password, email })
-    .returning();
-
-  res.json(user);
 };
